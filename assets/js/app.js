152f059f10bc0c4c647a3bc59c1e03b67a928924
aminami1127 <aminami1127@users.noreply.github.com>
2016-11-19 10:17:13 +0900

    add markers

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,99 @@
-function loadMap(country) {
-  var geocoder = new google.maps.Geocoder();
-  geocoder.geocode({'address' : country}, (results, status) => {
-        var center = results[0].geometry.location;
-        var mapOptions = {
-           center: center, zoom: 6,
-           mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+import $ from 'jquery';
+
+function loadMap(country, cities, paths, zoom) {
+  geocodePromise(country).then((latlang) => {
+    var mapOptions = {
+       center: latlang, zoom: zoom,
+       mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    // Markers
+    setMarkers(map, cities).then((cityLatLangs) => {
+      // Paths
+      setPaths(map, paths, cityLatLangs);
+    });
   });
 }
 
+function geocodePromise(address) {
+  var geocoder = new google.maps.Geocoder();
+  return new Promise((resolve) => {
+    geocoder.geocode({address : address}, (results, status) => {
+      resolve(results[0].geometry.location);
+    });
+  });
+}
+
+function setMarkers(map, cities) {
+  var cityLatLangs = [];
+  var icon = {
+      url: 'assets/img/Germany.png',
+      scaledSize: new google.maps.Size(50, 50), // scaled size
+  };
+  var dfd = $.Deferred();
+  cities.forEach((city, i) => {
+    geocodePromise(city).then((latlang) => {
+      new google.maps.Marker({
+        map: map,
+        position: latlang,
+        title: cities[i],
+        icon: icon
+      });
+      cityLatLangs.push(latlang);
+    });
+  });
+  dfd.resolve(cityLatLangs);
+  return dfd.promise();
+}
+
+function setPaths(map, paths, cityLatLangs) {
+  var lineSymbol = {
+    path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+  };
+  for (let path of paths) {
+      var line = new google.maps.Polyline({
+      map: map,
+      path: [cityLatLangs[path[0]], cityLatLangs[path[1]]],
+      strokeColor: 'red',
+      strokeOpacity: 0.5,
+      strokeWeight: 3,
+      icons: [{
+        icon: lineSymbol,
+        offset: '100%'
+      }],
+    });
+  }
+}
+
 function main() {
-  var country = "Germany";
-  loadMap(country);
+  var country = 'Germany';
+  var cities = [
+    'Munich',
+    'Fussen',
+    'Frankfurt',
+    'Cologne',
+    'Berlin',
+    'Potsdam',
+    'Dresden',
+    'Meissen',
+    'Leipzig',
+    'Nuremberg'
+  ];
+  var paths = [
+    [0, 1],
+    [0, 2],
+    [2, 3],
+    [2, 4],
+    [4, 5],
+    [5, 6],
+    [6, 7],
+    [6, 8],
+    [6, 9],
+    [9, 0],
+  ];
+  var zoom = 6;
+  loadMap(country, cities, paths, zoom);
 }
 
 window.onload = main;
